33d7cc92a2584535f19159f58c03fb5c74265a18
Piotr <piotrblonski96@gmail.com>
2021-03-14 15:52:56 +0100

    RNG

--- src/utils/graphs.py
+++ src/utils/graphs.py
@@ -1,5 +1,12 @@
 import networkx as nx
 import random
+import cv2 as cv
+import numpy as np
+import sys
+from itertools import combinations
+from tqdm import tqdm
+from scipy.spatial.distance import cdist
+from scipy.spatial import Delaunay
 def random_graph(num_nodes=5,prob_edge=0.3,seed=42):
     '''
 
@@ -10,7 +17,112 @@ def random_graph(num_nodes=5,prob_edge=0.3,seed=42):
     '''
     random.seed(seed)
     G = nx.fast_gnp_random_graph(num_nodes, prob_edge, seed=seed, directed=False)
+    center = len(G._node)
     for idx, node in enumerate(G._node):
-        G._node[idx] = {'x': random.randint(0,100),
-                        'y': random.randint(0,100)}
+        G._node[idx] = {'x': center+random.randint(-center,center),
+                        'y': center+random.randint(-center,center)}
     return G
+
+def get_keypoints(img,nFeatures=None):
+    if nFeatures is not None:
+        sift = cv.SIFT_create(nFeatures)
+    else:
+        sift = cv.SIFT_create()
+    kp, des = sift.detectAndCompute(img, None)
+    key_points = [key.pt for key in kp]
+    keyrelative_neighborhood_points = np.unique(key_points,axis=0)
+    return key_points
+
+def get_distances_between_keypoints(keypoints):
+    return cdist(keypoints, keypoints)
+
+def relative_neighborhood_baseline(keypoints):
+    G = nx.empty_graph()
+    for idx, key in enumerate(keypoints):
+        G.add_node(idx)
+        G._node[idx]={'x':key[0],
+                      'y':key[1]}
+    distance_matrix = get_distances_between_keypoints(keypoints)
+    comb = list(combinations(list(G.nodes),2))
+    #print(comb)
+    for combination in tqdm(comb):
+        can_add = True
+        #print(combination[0])
+        node_q = combination[0]
+        node_p = combination[1]
+
+        distances_q = distance_matrix[node_q]
+        distances_p = distance_matrix[node_p]
+
+        dist_q_p = distances_q[node_p]
+        # from q
+        closer_nodes_q = np.where(distances_q < dist_q_p)[0]
+        # from p
+        closer_nodes_p = np.where(distances_p < dist_q_p)[0]
+        #print('distance', dist_q_p)
+        for closer_node in closer_nodes_q:
+            if closer_node in closer_nodes_q and closer_node in closer_nodes_p:
+                #print(closer_nodes_p)
+                #print(closer_nodes_q)
+                can_add = False
+                break
+        if can_add:
+            #print('edge between', node_q,node_p,'distance',dist_q_p)
+            G.add_edge(node_q,node_p)
+    #print(distance_matrix)
+    return G
+
+
+def delaunay_triangulation(keypoints):
+    tri = Delaunay(keypoints)
+    G = nx.empty_graph()
+    for idx, key in enumerate(keypoints):
+        G.add_node(idx)
+        G._node[idx] = {'x': key[0],
+                        'y': key[1]}
+    print("delaunay_triangulation")
+    for vertexes in tqdm(tri.simplices):
+        vertexes = list(vertexes)
+        G.add_edge(vertexes[0], vertexes[1])
+        G.add_edge(vertexes[1], vertexes[2])
+        G.add_edge(vertexes[2], vertexes[0])
+    return G
+
+def delete_not_RNG_edges(G,keypoints=None):
+    vertices = keypoints
+    G_tmp = G.copy()
+    if keypoints is None:
+        vertices = []
+        for idx, key in enumerate(G_tmp._node):
+            x = G_tmp._node[idx]['x']
+            y = G_tmp._node[idx]['y']
+            vertices.append([x,y])
+    distance_matrix = get_distances_between_keypoints(vertices)
+    edges_to_delete = []
+    print("edges cleaning")
+    for edge in tqdm(G.edges):
+        node_q = edge[0]
+        node_p = edge[1]
+        distances_q = distance_matrix[node_q]
+        distances_p = distance_matrix[node_p]
+        dist_q_p = distances_q[node_p]
+        # from q
+        closer_nodes_q = np.where(distances_q < dist_q_p)[0]
+        # from p
+        closer_nodes_p = np.where(distances_p < dist_q_p)[0]
+        for closer_node in closer_nodes_q:
+            if closer_node in closer_nodes_q and closer_node in closer_nodes_p:
+                #print(closer_nodes_p)
+                #print(closer_nodes_q)
+                edges_to_delete.append(edge)
+                break
+    for edge in edges_to_delete:
+        node_q = edge[0]
+        node_p = edge[1]
+        G_tmp.remove_edge(node_q,node_p)
+    return G_tmp
+
+def relative_neighborhood(keypoints):
+    dt_graph = delaunay_triangulation(keypoints)
+    g = delete_not_RNG_edges(dt_graph,keypoints)
+    return g
--- src/utils/visualize.py
+++ src/utils/visualize.py
@@ -3,15 +3,18 @@ import numpy as np
 from PIL import Image, ImageDraw
 
 
-def draw_graph(G, radius=1, img=None) -> np.array:
+def draw_graph(G, radius=1, img_orginal=None,line_power = 1) -> np.array:
     '''
     :param G: networkx graph
     :param radius: int - radius how big must be the plotted node
     :param img: PIL Image - on which we will plot graph
     :return: numpy array with image
     '''
-    if img is None:
-        img = Image.new('RGB', (100, 100))
+    size = 2*len(G.nodes)
+    if img_orginal:
+        img = img_orginal.copy()
+    else:
+        img = Image.new('RGB', (size, size))
     draw = ImageDraw.Draw(img)
     points = []
     edges = []
@@ -30,11 +33,13 @@ def draw_graph(G, radius=1, img=None) -> np.array:
         node_B = G.nodes[e[1]]
         edges.append([node_A['x'], node_A['y'], node_B['x'], node_B['y']])
 
-    for points_pair in points:
-        draw.ellipse(points_pair, fill=(255, 0, 0, 255))
+
 
     for edge in edges:
-        draw.line(edge, fill=(255, 0, 0, 255))
+        draw.line(edge, fill=(255, 0, 0, 255),width=line_power)
+
+    for points_pair in points:
+        draw.ellipse(points_pair, fill=(255, 255, 255, 255))
 
     arr = np.asarray(img)
     img.close()
